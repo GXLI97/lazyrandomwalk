0a9525dc6c3cf271e1ec2be105b087c7ec7655ff
GXLI97 <gxli@princeton.edu>
2018-09-01 22:06:50 -0500

    wip

--- slurms/preprocess.sh
+++ slurms/preprocess.sh
@@ -4,6 +4,8 @@
 #SBATCH -c 4
 #SBATCH -t 10:00:00 # time required, here it is 1 min
 #SBATCH --mem=200G
+#SBATCH -o slurms/logs/preprocess.out # stdout is redirected to that file
+#SBATCH -e slurms/logs/preprocess.err # stderr is redirected to that file
 
 module load anaconda3
 src/preprocess.sh
