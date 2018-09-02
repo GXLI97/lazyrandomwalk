71b5b8b69d554a4d6f92769431fc005863027864
GXLI97 <gxli@princeton.edu>
2018-09-01 22:28:34 -0500

    wip

--- slurms/preprocess.sh
+++ slurms/preprocess.sh
@@ -2,8 +2,8 @@
 #SBATCH -N 1  # 1 node
 #SBATCH -n 1  # 1 task per node
 #SBATCH -c 4
-#SBATCH -t 10:00:00 # time required, here it is 1 min
-#SBATCH --mem=200G
+#SBATCH -t 5:00:00 # time required, here it is 1 min
+#SBATCH --mem=16G
 #SBATCH -o logs/preprocess.out # stdout is redirected to that file
 #SBATCH -e logs/preprocess.err # stderr is redirected to that file
 
