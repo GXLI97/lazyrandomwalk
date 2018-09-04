a8bc984baf190dbadc2809a7a85728b03b0024f6
GXLI97 <gxli@princeton.edu>
2018-09-03 20:24:14 -0500

    wip

--- src/svd.py
+++ src/svd.py
@@ -122,7 +122,7 @@ def main():
     logging.info("loading matrix from {}".format(MATRIX_FILE))
     X = load_npz(MATRIX_FILE)
     # convert to probability matrix
-    X.data = X.data/X.sum()
+    # X.data = X.data/X.sum()
 
     X = X.todense()
 
