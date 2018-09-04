bdc18a5052fc7de1a2e1d06326c9f5fc4dd6e7ae
GXLI97 <gxli@princeton.edu>
2018-09-03 20:01:45 -0500

    wip

--- src/run_glove.sh
+++ src/run_glove.sh
@@ -1,32 +1,37 @@
-CORPUS=text8
-DATA_DIR=data
-VECTOR_DIR=vectors
+# CORPUS=text8
+# DATA_DIR=data
+# VECTOR_DIR=vectors
+# GLOVE_BUILD_DIR=GloVe/build
+
+CORPUS=wikipedia
+DATA_DIR=/scratch/network/gxli/data
+VECTOR_DIR=/scratch/network/gxli/vectors
 GLOVE_BUILD_DIR=GloVe/build
 
 VOCAB_FILE=${DATA_DIR}/${CORPUS}.vocab.txt
 COOCCURRENCE_FILE=${DATA_DIR}/${CORPUS}.cooccurrence.bin
 COOCCURRENCE_SHUF_FILE=${DATA_DIR}/${CORPUS}.cooccurrence.shuf.bin
-VECTORS_FILE=${VECTOR_DIR}/${CORPUS}.glove.vectors.txt
+VECTORS_FILE=${VECTOR_DIR}/${CORPUS}.glove.vectors
 TEMP_FILE=${DATA_DIR}/${CORPUS}.temp_shuffle
 
-MEMORY=128
+MEMORY=16.0
 DIM=500
 
 $GLOVE_BUILD_DIR/shuffle \
-    -verbose $VERBOSE \
+    -verbose 2 \
     -memory $MEMORY \
-    -temp-file
+    -temp-file $TEMP_FILE \
     < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE
 
 $GLOVE_BUILD_DIR/glove \
     -verbose 2 \
     -vector-size $DIM \
-    -input-file $COOCCURRENCE_SHUF_FILE
-    -vocab-file $VOCAB_FILE
+    -input-file $COOCCURRENCE_SHUF_FILE \
+    -vocab-file $VOCAB_FILE \
     -save-file $VECTORS_FILE
 
 cd GloVe
 
 python3 ./eval/python/evaluate.py \
     --vocab_file ../$VOCAB_FILE \
-    --vectors_file ../$VECTORS_FILE
+    --vectors_file ../$VECTORS_FILE.txt
